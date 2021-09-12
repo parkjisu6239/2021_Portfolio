954d91887b8ca89945fd2ed81dd58785a5f311cd
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-12 19:37:55 +0900

    feat: Add menu
    메뉴 추가

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import { useState } from "react"
+import Concept from "../components/home/concept/Concept";
+import Greeting from "../components/home/Greeting/Greeting";
+import PeopleSay from "../components/home/peopleSay/PeopleSay";
+import Portfolio from "../components/home/portfoliio/Portfolio";
+import Skill from "../components/home/skill/Skill";
+import Specialize from "../components/home/specialize/Skill";
 
 function HomePage() {
   const [ isLoading, setIsLoading ] = useState(true); // 나중에 로딩페이지가 있다면 사용
   return (
     <main>
-      <h1>홈페이지다</h1>
+      <Greeting/>
+      <Concept/>
+      <Portfolio/>
+      <Specialize/>
+      <Skill/>
+      <PeopleSay/>
     </main>
   )
     
