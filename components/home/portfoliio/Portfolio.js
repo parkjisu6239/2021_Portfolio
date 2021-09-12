300aabfd44da8ad63d94b63b05736a35ee426047
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-12 19:39:54 +0900

    feat: Add Menus
    메뉴 페이지별 최상위 컴포넌트 생성

---
+++ components/home/portfoliio/Portfolio.js
@@ -0,0 +1,11 @@
+export default function Portfolio() {
+    return (
+        <article>
+            내 작업들이야, 더 많은 내용은 위에 메뉴에 있단다.
+            여기는 하이라이팅 이랄까 한 두세게만 보여줄게
+            작업 리스트
+            작업 아이템 3~4개
+            더보기(모든 작업 보기)
+        </article>
+    )
+}
