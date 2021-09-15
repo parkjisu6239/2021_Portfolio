4ac754c50ed02eb158ef30db4cfcee3b5d296beb
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-15 22:46:31 +0900

    feat: Delete unused code

--- src/components/home/concept/ConceptItem.js
+++ src/components/home/concept/ConceptItem.js
@@ -1,8 +1,6 @@
 import styles from './ConceptItem.module.css'
 
 export default function ConceptItem({ item, clickItem, selectedItem }) {
-    const image = require('../../../assets/concept/Fire_perspective_matte_s.png')
-
     return (
         <div onClick={() => clickItem(item)} className={`${styles.conceptitem} ${item === selectedItem && styles.active}`}>
             <div>
