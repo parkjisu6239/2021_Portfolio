037f067488a053ce00973405236da6c250a8b257
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-24 22:28:07 +0900

    feat: Add sample image

--- src/components/home/greeting/ProfileImage.js
+++ src/components/home/greeting/ProfileImage.js
@@ -4,7 +4,7 @@ import styles from './ProfileImage.module.css'
 export default function ProfileImage() {
     return (
         <section className={styles.profileImage}>
-            이미지 있다치고
+            <img src={ `assets/home/hello.png` }/>
         </section>
     )
 }
