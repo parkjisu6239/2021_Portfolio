9a80f3087baec57b88edcd4ec39f5c719f887783
parkjisu6239 <parkjisu6239@gmail.com>
2021-09-26 23:05:30 +0900

    build: Add base url for deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Top/>
         <Switch>
