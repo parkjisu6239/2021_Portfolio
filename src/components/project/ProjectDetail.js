70a384836a6f3fe8d4cf464a25c76530ac571541
parkjisu6239 <parkjisu6239@gmail.com>
2021-10-08 23:13:12 +0900

    feat: Add project detail page
    router params is project id. this page show project readme

--- src/components/project/ProjectDetail.js
+++ src/components/project/ProjectDetail.js
@@ -1,10 +1,21 @@
+import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
+import MarkdownRenderer from "../common/MarkdownRenderer"
+import ProjectItem from "./ProjectItem"
 
 export default function ProjectDetail() {
     const id = useParams().id
+    const project = require('../../db/project.json')[id]
+
     return (
-        <article>
-            { id }번 프로젝트 디테일
-        </article>
+        <StyledProjectDetail>
+            { id }번 프로젝트 디테일, {project.title} / {project.eng_title}
+            {project.tag}
+            <MarkdownRenderer/>
+        </StyledProjectDetail>
     )
 }
+
+const StyledProjectDetail = styled.article`
+    padding: 50px 15vw;
+`
